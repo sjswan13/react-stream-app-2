0db2914de8734b7fdfc39e626459c9a8fddc22d8
sjswan13 <sjswan13@gmail.com>
2024-09-17 16:14:17 -0600

    React chat tutorial

--- react-stream-chat-app/src/App.js
+++ react-stream-chat-app/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat, 
+  Channel, 
+  Window,
+  ChannelHeader,
+  MessageList, 
+  MessageInput,
+  Thread, 
+  LoadingIndicator,
+} from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
 
-function App() {
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY;
+
+const user = {
+  id: 'jane', 
+  name: 'Jane',
+  image: 'https://getstream.io/random_svg/?name=Jane',
+};
+
+export default function App() {
+  const [client, setClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    if (!apiKey) {
+      console.error('API key is not set. Make sure your environment variable is configured properly.');
+      return;
+    }
+
+    const chatClient = StreamChat.getInstance(apiKey);
+
+    const connectUser = async () => {
+      try {
+        
+        const devToken = chatClient.devToken(user.id);
+
+        
+        await chatClient.connectUser(user, devToken);
+
+        const channel = chatClient.channel('messaging', 'react-talk', {
+          image: 'https://www.drupal.org/files/project-images/react.png',
+          name: 'Talk about React',
+          members: [user.id],
+        });
+
+        await channel.watch();
+
+        setChannel(channel);
+        setClient(chatClient);
+      } catch (error) {
+        console.error('Error connecting user:', error);
+      }
+    };
+
+    connectUser();
+
+    
+    return () => {
+      if (chatClient) {
+        chatClient.disconnectUser();
+      }
+    };
+  }, []); 
+
+  if (!channel || !client) return <LoadingIndicator />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client} theme='messaging light'>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
 }
-
-export default App;
